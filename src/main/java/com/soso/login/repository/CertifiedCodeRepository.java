8f61a96cceefe65551f807cccec02ed114523b6a
전민수 <79839332+alstntjddms@users.noreply.github.com>
2023-11-23 21:31:37 +0900

    주석 삭제

--- src/main/java/com/soso/login/repository/CertifiedCodeRepository.java
+++ src/main/java/com/soso/login/repository/CertifiedCodeRepository.java
@@ -14,14 +14,12 @@ public class CertifiedCodeRepository {
     // 10분마다 repository의 CertifiedCodeDTO의 sendDate요소가 (현재시간-3분)보다 과거이면 삭제한다.
     @Scheduled(fixedRate = 60 * 10 * 1000) // 600,000 밀리초 = 10분
     public void updateCertifiedCodeRepository() {
-        System.out.println("실행");
         long threeMinutesAgo = System.currentTimeMillis() - (60 * 3 * 1000);
 
         for (Iterator<CertifiedCodeDTO> iterator = repository.iterator(); iterator.hasNext();) {
             CertifiedCodeDTO certifiedCode = iterator.next();
             if (isCertifiedCodeExpired(certifiedCode, threeMinutesAgo)) {
                 iterator.remove();
-                System.out.println("certifiedCode = " + certifiedCode + "가 삭제되었습니다.");
             }
         }
     }
